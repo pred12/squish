cc4178f3baa8b1b7c41a68879e2233ecfc238ea3
Preston Allred <43273024+pred12@users.noreply.github.com>
2021-07-27 12:23:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -142,4 +142,4 @@ async def on_message(message):
     await client.(int(splitMessage[1])).kick(splitMessage[2])
 
 
-client.run(str(secrets.SECRET_TOKEN))
+client.run("ODY0MzMwMTkwNTAyODg3NDY0.YOz4Nw.y7xfMQ-MHVADA7-FdJXZIjjZ8bI")
